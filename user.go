8702ce61e75940663112961541ec81ba39ebecf8
Salar Moghaddam <salarmgh@gmail.com>
2020-06-15 10:21:09 +0430

    add account objectclass

--- user.go
+++ user.go
@@ -16,7 +16,7 @@ func (l *LDAP) AddUser(name string, email string, password string) error {
 	var attrs []ldap.Attribute
 	attr := ldap.Attribute{
 		Type: "objectClass",
-		Vals: []string{"top", "person", "uidObject", "simpleSecurityObject"},
+		Vals: []string{"top", "account", "person", "simpleSecurityObject"},
 	}
 	attrs = append(attrs, attr)
 
